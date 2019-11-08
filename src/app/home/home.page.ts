f814fe7afc12444539eee03b731781dfb59539cb
Felipe Victor <felipe@oitsys.com>
2019-11-08 14:50:34 -0600

    Slides animations added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
+import { slidesCoverflow, slidesCube, slidesFlip } from './slides-animations';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
+
+
 export class HomePage {
+  slides: any;
+  slideOpts: any;
 
-  constructor() {}
+  constructor() {
 
+    // this.slideOpts = slidesCoverflow;
+    this.slideOpts = slidesCube;
+    // this.slideOpts = slidesFlip;
+
+    this.slides = [
+      {
+        title: 'Listen your favorite music',
+        description:
+          'Los mejores álbums y canciones. Escucha y comparte en cualquier momento a todas horas.',
+        icon: 'play'
+      },
+      {
+        title: 'Listen your favorite music',
+        description:
+          'Los mejores álbums y canciones. Escucha y comparte en cualquier momento a todas horas.',
+        icon: 'play'
+      },
+      {
+        title: 'Listen your favorite music',
+        description:
+          'Los mejores álbums y canciones. Escucha y comparte en cualquier momento a todas horas.',
+        icon: 'play'
+      },
+      {
+        title: 'Listen your favorite music',
+        description:
+          'Los mejores álbums y canciones. Escucha y comparte en cualquier momento a todas horas.',
+        icon: 'play'
+      }
+    ];
+  }
 }
